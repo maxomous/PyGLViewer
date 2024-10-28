2c5fd3026ed533583b48acf3e636056412d591ec
maxomous <m.s.peglar@gmail.com>
2024-10-28 00:33:04 +0000

    Added comments

--- main.py
+++ main.py
@@ -15,28 +15,30 @@ class Application(BaseApplication):
         self.init_lights()
 
     def init_geometry(self):
+        # Settings
         segments = 32
-        self.renderer.add_grid(10, 1, Color.GRAY)
-        self.renderer.add_axis(size=1)
-
-        # Row 1 (all wireframe with consistent thickness, different colors)
         point_size = 7.0
         line_thickness = 3.0
-
+        
+        # Grid and axis
+        self.renderer.add_grid(10, 1, Color.GRAY)
+        self.renderer.add_axis(size=1)
+        
+        # Row 1 - Wireframe Shapes
         self.renderer.add_point((-4, 4, 0), Color.RED, point_size=point_size)
         self.renderer.add_line((-2.5, 3.5, 0), (-1.5, 4.5, 0), Color.ORANGE, line_width=line_thickness)
         self.renderer.add_triangle((0, 4.433, 0), (-0.5, 3.567, 0), (0.5, 3.567, 0), wireframe_color=Color.YELLOW, show_wireframe=True, show_body=False, line_width=line_thickness)
         self.renderer.add_rectangle((2, 4), 1, 1, wireframe_color=Color.GREEN, show_wireframe=True, show_body=False, line_width=line_thickness)
         self.renderer.add_circle(position=(4, 4, 0), radius=0.5, segments=segments, wireframe_color=Color.BLUE, show_body=False, line_width=line_thickness)
 
-        # Row 2 (filled shapes)
+        # Row 2 - Filled Shapes
         self.renderer.add_circle(position=(-4, 2, 0), radius=0.5, segments=segments, color=Color.GREEN, show_body=True)
         self.renderer.add_cube(Color.RED, translate=(-2, 2, 0.5), scale=(0.5, 0.5, 0.5), rotate=(np.pi/4, np.pi/4, 0), buffer_type=BufferType.Dynamic, show_body=True)
         self.renderer.add_cone(Color.rgb(255, 165, 0), segments=segments, translate=(0, 2, 0.5), scale=(0.5, 0.5, 0.5), show_body=True)
         self.renderer.add_cylinder(Color.WHITE, segments=segments, translate=(2, 2, 0.5), scale=(0.5, 0.5, 0.5), show_body=True)
         self.renderer.add_sphere(translate=(4, 2, 0.5), radius=0.25, subdivisions=4, color=Color.WHITE)
 
-        # Row 3 (filled shapes)
+        # Row 3 - Filled Shapes
         self.renderer.add_cube(Color.YELLOW, translate=(-4, 0, 0.5), scale=(0.5, 0.5, 0.5), buffer_type=BufferType.Dynamic, show_body=True)
         self.renderer.add_arrow((-2.4, -0.4, 0.25), (-1.6, 0.4, 0.75), shaft_radius=0.2, head_radius=0.4, head_length=0.3, color=Color.RED, show_body=True)
 
@@ -77,8 +79,8 @@ class Application(BaseApplication):
 
     def render_ui(self):
         """Called by BaseApplication.render()"""
-        self.render_debug_window()
         imgui.show_demo_window()
+        self.render_debug_window()
 
 if __name__ == '__main__':
     
